09414f5fc0403c108ff3846adcecd715ae1692ca
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2023-07-28 01:06:11 +0900

    [level 2] Title: 피보나치 수, Time: 6.54 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    
+    // 초기값 n은 2이상이여야 하기 때문에 2로 설정
+    let initNum = 2;
+    
+    // 배열에 2이하일때의 값을 넣어준다.
+    const array = [0,1];
+    
+    //while문을 이용하여 n값이 될때 까지 현재값 그 전의 값을 더하여 array 배열에 넣어준다
+    while(initNum < n+1){
+        const curNum = array[initNum-1] + array[initNum-2] //피보나치 수를 계산한다.
+        array.push(curNum%1234567) // 피보나치의 값을 array에 넣어준다.
+        initNum++
+    }
+    return array[n] // 주어진 피보나치수에 1234567을 나눈 나머지를 반환한다.
+}
