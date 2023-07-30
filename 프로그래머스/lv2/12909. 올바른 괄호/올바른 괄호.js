b608e3aa7762d07cfa1b6d8f551994b49f4b46b6
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2023-07-31 01:54:59 +0900

    [level 2] Title: 올바른 괄호, Time: 8.67 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,3 +1,35 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
 function solution(s){
     const s_array = s.split("")
     const s_left = s.split("(").length-1 
@@ -5,7 +37,7 @@ function solution(s){
     let a = 0
     let b = 0
     let c = true
-    
+
     let aa = s_array.map((x) => {
         if(x == "("){
             a++
@@ -15,13 +47,13 @@ function solution(s){
         if(a < b){c = false}
     })
 
-    
+
 
     const s_first = s_array.slice(0,1) == "("
     const s_final = s_array.slice(-1) == ")"
     const s_length = s.length % 2 == 0 ? true : false
     const s_number = s_left == s_right ?  true : false
-    
+
     if( s_first && s_final && s_length && s_number && c){
         return true
     }else{
