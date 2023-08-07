b6b079d8fd3b402b7619413d7833e53d9af2a52b
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2023-08-08 01:54:37 +0900

    [level 2] Title: 짝지어 제거하기, Time: 40.72 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,20 @@
+function solution(s){
+    
+    // 주어진 문자열이 홀수이면 어차피 1개가 남으므로 무조건 result 0이나옴
+   if(s.length % 2 !== 0){
+       return 0
+   }
+    
+    const array = [];
+    
+    for(item of s){
+        array.push(item);
+        // console.log(array)
+        if(array[array.length - 2] === array[array.length - 1]){
+            array.pop()
+            array.pop()
+        }
+        
+    }
+    return array[0] ? 0 :1;
+}
