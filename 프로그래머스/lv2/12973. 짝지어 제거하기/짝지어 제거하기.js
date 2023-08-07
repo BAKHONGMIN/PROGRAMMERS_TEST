1b44e1a1c74289050d0199bbcbd3ecee43b94742
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2023-08-08 02:01:54 +0900

    [level 2] Title: 짝지어 제거하기, Time: 23.08 ms, Memory: 46.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -5,9 +5,11 @@ function solution(s){
        return 0
    }
     
-    // 
+    // 반본문에서 현재 값을 넣을 배열
     const array = [];
     
+    // 반복문을 돌면서 현재값을 array에 집어 넣고 그 전의 값과 비교하여 
+    // 같으면 array 배열에서 현재 값과 그전의 값을 제거하고 반복한다.
     for(let i = 0; i < s.length; i++){
         array.push(s[i]);
         if(array[array.length - 2] === array[array.length - 1]){
