934832566f0b14f6b7d9dc8d9cb51ad0331eb411
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-03-07 00:46:54 +0900

    [level 2] Title: 할인 행사, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,10 +1,15 @@
 function solution(want, number, discount) {
     
-    // 내가 원하는 상품과 개수
+    // 내가 원하는 상품과 개수의 object
     const myItem = {};
+    
+    //내가 원하는 제품의 총 개수
     let myNumber = 0;
+    
+    // 가능한 날짜
     let day = [];
     
+    // 원하는 상품과 원하는 개수를 조합하여 object 생성
     for(let i = 0; i < want.length; i++){
         const item = want[i];
         const quantity = number[i];
@@ -12,7 +17,7 @@ function solution(want, number, discount) {
         myItem[item] = quantity;
     }
     
-    //정렬시키기
+    //나의 구매 희망 리스트 object 정렬시키기
     const myKeys = Object.keys(myItem);
     myKeys.sort()
     const mySort = {}
@@ -20,17 +25,23 @@ function solution(want, number, discount) {
         mySort[key] = myItem[key]
     })
     
-    //세일 상품
+    //세일 상품의 총수
     const idx = discount.length;
+    
+    // 내가 원하는 날짜와 세일상품의 총수 제한
     const limit = idx - myNumber;
     
     for(let i = 0; i <= limit; i++){
+        // 내가 원하는 물건의 개수와 세일 개수를 맞추기 위해 잘라서 테스트
         const list = discount.slice(i, myNumber + i)
+        
+        // 자른 배열을 object형태로 생성
         const disList = list.reduce((acc, item) => {
             acc[item] = (acc[item] || 0) + 1;
             return acc
         }, {})
         
+        // discount 정렬 시키기
         const disKeys = Object.keys(disList);
         disKeys.sort()
         const disSort = {}
@@ -38,6 +49,7 @@ function solution(want, number, discount) {
             disSort[key] = disList[key]
         })
         
+        // 내가 원하는 물건의 이름과 개수가 세일 물건 날짜와 개수가 맞는지 테스트
         const myList = JSON.stringify(mySort);
         const dList = JSON.stringify(disSort);
         
