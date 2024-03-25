c2ca2a280fd9b0d2b9dd5e28fa41fd9539d96a92
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-03-26 01:59:18 +0900

    [level 2] Title: 괄호 회전하기, Time: 2.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,11 +1,14 @@
 function solution(s) {
     
+    // 조건이 맞으면 카운트
     let count = 0;
+    
+    // 문자열 저장
     let strings = s;
     
     for(let i=0; i<s.length; i++){
         let destrings = strings;
-        for(let j = 0; j<s.length; j++){
+        for(let j = 0; j<s.length/2; j++){
             destrings = destrings.replace(/\[\]/g, "").replace(/\(\)/g, "").replace(/\{\}/g, "");    
         }
         console.log(destrings) 
