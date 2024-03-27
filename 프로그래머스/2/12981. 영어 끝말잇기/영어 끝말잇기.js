91f39fa482a21b2a26ff5a56c540f734b5624348
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-03-28 01:14:53 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,19 +1,29 @@
 function solution(n, words) {
-    const set = new Set();
-    let prevWord = words[0];
-    let idx = 0;
-
-    for (let i = 0; i < words.length; i++) {
-        const word = words[i];
-        
-        if (set.has(word) || prevWord[prevWord.length - 1] !== word[0]) {
-            idx = i;
-            break;
+    
+    const array = [...new Set(words)];
+    let count = 0;
+    const check = words.reduce((acc, cur, idx) => {
+        if(acc[acc.length-1] !== cur[0]){
+            words.splice(1)
+            count++
+            return idx+1
         }
-
-        set.add(word);
-        prevWord = word;
+        
+        if(cur !== array[idx]){
+            words.splice(1)
+            count++
+            return idx+1
+        }
+        
+        return acc+cur
+    })
+    if(count === 0){
+        return [0,0]
+    }else{
+        const a = check % n === 0 ? n : check % n;
+        const b = Math.ceil(check / n);
+    
+        return [a, b];
     }
-
-    return idx === 0 ? [0, 0] : [(idx % n) + 1, Math.ceil((idx + 1) / n)];
+    
 }
