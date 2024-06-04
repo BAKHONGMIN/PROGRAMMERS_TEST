42c7f2bfe9b82b911da08b45641a594d851c0103
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-06-04 22:52:23 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,18 +1,19 @@
 function solution(progresses, speeds) {
-    const finish = [];
-    const result=[];
-    let i = 0;
+    const finish = []; // 배포가능 날짜 배열
+    const result=[]; // 배포가능 개수
+    let i = 0; // 배포 가능 개수 시작값
     
+    // 배포가능 날짜 구하기
     for(let i in progresses){
         const day = (100 - progresses[i])/speeds[i]
         finish.push(Math.ceil(day))
     }
     
-
+    // 배포가능 개수 구하기
     while (i < finish.length) {
         let count = 1;
         for (let j = i + 1; j < finish.length; j++) {
-            if (finish[j] > finish[i]) {
+            if (finish[i] < finish[j]) {
                 break;
             }
             count++;
