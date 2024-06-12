b97a99418bdaebc3f8ab63b697fc7d85e202d65c
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-06-13 01:30:58 +0900

    [level 2] Title: 피로도, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,7 +1,8 @@
 function solution(k, dungeons) {
-    const array = [];
-    const result = [];
+    const array = []; // dungeons의 모든 경우의 수를 집어 넣는 배열
+    const result = []; // 모든 경우의 수에서 던전 통과 수를 넣는 배열
     
+    // 즉시 함수로 모든 경우의 수를 array 배열에 넣기
     (function allArr(subArr, m = []) {
         if (subArr.length === 0) {
             array.push(m);
@@ -14,6 +15,7 @@ function solution(k, dungeons) {
         }
     })(dungeons);
     
+    // 모든 경우의 수에서 던전 통과 개수를 result 배열에 넣기
     for(let i=0; i<array.length; i++){
         let num = 0;
         let knum = k;
