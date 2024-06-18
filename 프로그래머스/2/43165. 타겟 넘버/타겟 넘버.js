0de04c0c87bf01f5e0a8692fe4045ee2350426c3
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-06-19 01:23:13 +0900

    [level 2] Title: 타겟 넘버, Time: 4.47 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -7,14 +7,20 @@ function solution(numbers, target) {
 
     // ways의 배열이 빌때까지 반복
     while (array.length > 0) {
+        
+        // 배열의 마지막 요소를 꺼내어 현재 합계와 인덱스를 가져온다.
         let { sum, index } = array.pop();
 
+        // 모든 숫자를 다 사용한 경우
         if (index === numbers.length) {
+            // 현재 합계가 목표 숫자와 같은지 확인
             if (sum === target) {
                 count++;
             }
         } else {
+             // 다음 숫자를 더한 경우를 배열에 추가
             array.push({ sum: sum + numbers[index], index: index + 1 });
+            // 다음 숫자를 뺀 경우를 배열에 추가
             array.push({ sum: sum - numbers[index], index: index + 1 });
         }
     }
