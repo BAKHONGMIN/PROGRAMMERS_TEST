dc59246e308fdd3bc01e1eb4b7abeeaaeda3ef46
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-07-02 01:20:22 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,14 +1,26 @@
 function solution(word) {
-    let maxNum = 0;
-    let numLen = 5;
     let order = "AEIOU";
     let result = 0;
+    let maxNum = 0;
     
-    for(let i = 1; i<=numLen; i++){maxNum+=order.length**i}
-    for(let i = 1; i<=numLen; i++){
-        const cases =  Math.floor(maxNum/order.length**i)
-        const num = order.indexOf(word[i-1])+1
-        num == 0 ? result+=0 : num == 1 ? result+=1 : result+=cases*(num-1)+1
+    // 길이 1부터 5까지의 단어로 구성할 수 있는 최대 개수를 계산
+    for (let i = 1; i <= 5; i++) {
+        maxNum += Math.pow(order.length, i);
     }
-    return result
+    
+    // 주어진 단어의 순서를 계산
+    for (let i = 0; i < word.length; i++) {
+        let num = order.indexOf(word[i]); // 현재 문자의 순서를 찾음
+        let cases = 0;
+        
+        // 각 위치에서 시작하는 가능한 경우의 수를 계산
+        for (let j = 4; j > i; j--) {
+            cases += Math.pow(order.length, j - i);
+        }
+        
+        // 현재 문자로 시작하는 경우의 수를 누적
+        result += num * (cases + 1) + 1;
+    }
+    
+    return result;
 }
