2f856cc577519c33a3b6d2f407dade20ac863b71
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-07-02 01:16:59 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -8,9 +8,7 @@ function solution(word) {
     for(let i = 1; i<=numLen; i++){
         const cases =  Math.floor(maxNum/order.length**i)
         const num = order.indexOf(word[i-1])+1
-        console.log(cases, num)
-        num == 1 ? result+=1 : num == 0 ? result+=0 : result+=cases*(num-1)+1
+        num == 0 ? result+=0 : num == 1 ? result+=1 : result+=cases*(num-1)+1
     }
     return result
-
 }
